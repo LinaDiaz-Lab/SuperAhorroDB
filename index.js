c8f62f737f621f9ba3558ba19bba5c81822ee936
LinaDiaz-Lab <linamariadiaz@outlook.es>
2020-11-26 19:44:40 -0500

    Primer commit

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
+
+require('./baseDeDatos')
+//rutas
 const rutasClientes = require('./rutas/cliente')
 const rutasFacturas = require('./rutas/factura')
 const rutasProductos = require('./rutas/producto')
 const rutasUsuarios = require('./rutas/usuario')
 const {PORT} = require('./config')
 
-require('./baseDeDatos')
-
 app.use(cors())
 app.use(bodyParser.json())
 
+//rutas, servidor
 app.use('/api/clientes', rutasClientes)
 app.use('/api/facturas', rutasFacturas)
 app.use('/api/productos', rutasProductos)
